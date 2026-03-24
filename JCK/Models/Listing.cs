bf63a2923f4ac5cd130d9062c3428b5328fa3f68
Callum <g00432230@atu.ie>
2026-03-24 10:50:54 +0000

    Added Details for listing

--- JCK/Models/Listing.cs
+++ JCK/Models/Listing.cs
@@ -2,6 +2,22 @@ namespace JCK.Models;
 
 public class Listing
 {
+    //Car info
     public int Id { get; set; }
     public string CarName { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Year { get; set; }
+
+    // Location
+    public string Location { get; set; } = string.Empty;
+
+    // Availability
+    public bool IsAvailable { get; set; } = true;
+
+    //Image
+    public string ImageUrl { get; set; } = string.Empty;
+
+    //Car Owner
+    public string OwnerId { get; set; } = string.Empty;
+    
 }
