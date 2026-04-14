235e536b1e9f37d21b998f0b3e9e0aecc220592c
totally real name <153068435+maks-ymilian@users.noreply.github.com>
2026-04-14 19:34:11 +0100

    frontend for home page

--- JCK/wwwroot/js/home-page.js
+++ JCK/wwwroot/js/home-page.js
@@ -5,21 +5,41 @@ export async function fetchListings() {
     try {
         const response = await fetch(apiUrl);
         if (!response.ok) throw new Error(`${response.status}`);
-        const listings = await response.json();
-
-        const listEl = document.getElementById('listingList');
-        listEl.innerHTML = ''; // Clear existing content
-
-        listings.forEach(item => {
-            const li = document.createElement('li');
-            li.textContent = `${item.carName} - $${item.id}`;
-            listEl.appendChild(li);
-        });
+        return await response.json();
 
     } catch (err) {
         console.error('Error fetching listings:', err);
     }
 }
 
-// Fetch listings when page loads
-document.addEventListener('DOMContentLoaded', fetchListings);
+const listing_container = document.getElementById("listing-container");
+const title = document.getElementById("title");
+
+const listings = await fetchListings();
+listings.forEach(item => {
+    listing_container.insertAdjacentHTML("beforeend", `
+        <a href="${"/listing"}">
+            <div class="card" style="padding: 16px; gap: 7px; display: flex; flex-direction: column">
+                <img src="${item.imageUrl}" style="width: 100%; aspect-ratio: 1 / 1; border-radius: 16px">
+                <p style="margin: 0px; font-size: 16px">${item.carName}</p>
+                <p style="margin: 0px; font-size: 14px; color: #676767">${"22 - 24 May"}<br/>&euro;${item.price} &middot; <svg style="height: 10px; width: 10px; fill: #676767"><use href="#star"></use></svg> ${item.review}</p>
+            </div>
+        </a>
+    `);
+});
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+        fetch(`https://api.tomtom.com/search/2/reverseGeocode/${position.coords.latitude},${position.coords.longitude}.json?key=6YBNUuGNfg3XDVzZ96l62pEKJUGlJk34&radius=100`)
+            .then((response) => {
+                if (!response.ok)
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                return response.json();
+            })
+            .then((data) => {
+                title.innerHTML = "Cars in " + data.addresses[0].address.municipality;
+            })
+            .catch((error) => console.error("Error fetching reverse geocode:", error));
+    });
+} 
+
