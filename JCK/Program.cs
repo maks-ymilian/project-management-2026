1dc948cd67125f198f8203d107f01e0ee80386ca
Roanbradley <roanbradley5@gmail.com>
2026-04-17 21:59:03 +0100

    Final Stripe configuration setup git please work

--- JCK/Program.cs
+++ JCK/Program.cs
@@ -3,6 +3,16 @@ using JCK.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// make sure to set stripe key in eviroment variables
+var stripeKey = Environment.GetEnvironmentVariable("STRIPE_SECRET_KEY");
+
+if (string.IsNullOrEmpty(stripeKey))
+{
+    throw new Exception("Missing Stripe key");
+}
+
+Stripe.StripeConfiguration.ApiKey = stripeKey;
+
 // register the AppDbContext which holds the tables
 var connectionString = Environment.GetEnvironmentVariable("SUPABASE_CONNECTION_STRING");
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -10,6 +20,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 Console.WriteLine("Connection string: " + connectionString);
 
 builder.Services.AddControllers(); // <-- Required
+
+
 
 var app = builder.Build();
 
